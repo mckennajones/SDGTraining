40f14e168c9365e629c7681b68dd8f33d9e3bb13
mckennajones <mjones0014@gmail.com>
2016-04-11 13:58:04 -0700

    Started testing project

---
+++ PeopleProTraining/PeopleProTesting/BuildingsControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeopleProTesting.Controllers;
+
+namespace PeopleProTesting.Controllers
+{
+    [TestClass]
+    public class BuildingsControllerTest
+    {
+        [TestMethod]
+        public void TestCreateView()
+        {
+            var controller = new BuildingsController();
+        }
+    }
+}
